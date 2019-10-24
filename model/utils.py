ebe9b184a3ede5ef1f9f4c0754182f674ce04ca6
markkvdb <markvanderbroek@gmail.com>
2019-10-24 12:33:37 +0200

    Added experiment_runner to simplify API

---
+++ model/utils.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from itertools import product
+
+from model.sim_components import Simulator
+
+
+def experiment_runner(settings, sim_time, time_delta):
+  """
+  Runs all model experiments with all different combinations of provided 
+  settings values.
+
+  Keyword arguments:
+    settings -- Dictionary with all settings
+  """
+  
+  # Create all possible combinations of setting values
+  settings_names = sorted(settings)
+  settings_comb = list(product(*(settings[name] for name in settings_names)))
+
+  sim_dfs = pd.DataFrame()
+
+  # Run experiment for every combination
+  for settings_vals in settings_comb:
+    settings_experiment = dict(zip(settings_names, settings_vals))
+
+    # Run simulation
+    simulator = Simulator(time_delta, sim_time, settings_experiment)
+    simulator.run()
+            
+    # Save output to master data frame
+    sim_data = simulator.create_output_df()
+    for setting, value in settings_experiment.items():
+      sim_data[setting] = value
+
+    sim_dfs = sim_dfs.append(sim_data)
+
+  # Change time column
+  sim_dfs.time = sim_dfs.time * time_delta
+  
+  return(sim_dfs)
+
+
+
+
